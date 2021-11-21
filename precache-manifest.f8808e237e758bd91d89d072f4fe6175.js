6a1de6469641c213406051febb5c24853b9d7db6
32byte <xlebedenko@gmail.com>
2021-11-21 19:40:33 +0100

    Updates

---
+++ precache-manifest.f8808e237e758bd91d89d072f4fe6175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f821764eef4be20d51453dc84b1d3c8",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "e36832b52d808172f865",
+    "url": "/tictactoe/static/css/main.efad5ace.chunk.css"
+  },
+  {
+    "revision": "b9e87e82311f7c9ad9aa",
+    "url": "/tictactoe/static/js/2.e1c6e1e6.chunk.js"
+  },
+  {
+    "revision": "1f050c4739cc0e051e098da427ecc9de",
+    "url": "/tictactoe/static/js/2.e1c6e1e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e36832b52d808172f865",
+    "url": "/tictactoe/static/js/main.0e75eae7.chunk.js"
+  },
+  {
+    "revision": "8fd1009f88de2a8f7d1e",
+    "url": "/tictactoe/static/js/runtime-main.57baa19c.js"
+  },
+  {
+    "revision": "cb82ab9f5d85ade636465aa6c6278fe4",
+    "url": "/tictactoe/static/media/logo.cb82ab9f.png"
+  }
+]);
